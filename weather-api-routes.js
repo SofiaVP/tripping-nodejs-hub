7564c1f2a980c89a2b338e3f464c330c4016c306
SofiaVP <sofiavayas@hotmail.com>
2020-06-20 19:11:09 +0200

    link between angular and nodeJS --> okgit add weather-api-routes.js

--- weather-api-routes.js
+++ weather-api-routes.js
@@ -1,8 +1,27 @@
 var express = require('express');
 const request = require('request');
 const apiRouter = express.Router();
+const fetch = require("node-fetch");
 
 var myGenericMongoClient = require('./my_generic_mongo_client');
+
+
+//const fetch = require("node-fetch");
+//const url = "https://jsonplaceholder.typicode.com/posts/1";
+
+const getData = async url => {
+	try {
+	  const response = await fetch(url);
+	  const json = await response.json();
+	  //console.log(json);
+	  //exports json = json;
+	  //res.send(json)
+	} catch (error) {
+	  console.log(error);
+	}
+  };
+
+
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 function replace_mongoId_byCode(meteo){
@@ -17,7 +36,59 @@ function replace_mongoId_byCode_inArray(meteoArray){
 	}
 	return meteoArray;
 }
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++++++++++++++++++++++++++++++++++++//
+
+
+// JUST GET CURRENT WEATHER FROM API 
+// exemple URL: http://localhost:8282/weather-api/current/toulouse
+apiRouter.route('/weather-api/current/:name')
+.get(async function(req, res, next){
+	var name = req.params.name; 
+	const url = "http://api.openweathermap.org/data/2.5/weather?q="+name+"&appid=2d0c2252a4bc29b72d473be3efad81e1&units=metric";
+	const response = await fetch(url);
+	const json = await response.json();
+	res.send(
+		{
+			name: json.name, 
+			dt: json.dt, 
+			temp: json.main.temp,
+			feels_like: json.main.feels_like,   
+			sunrise: json.sys.sunrise, 
+			sunset: json.sys.sunset
+		 })
+
+	
+	// const getData = async url => {
+	// 	try {
+	// 	  const response = await fetch(url);
+	// 	  const json = await response.json();
+	// 	  console.log(json);
+	// 	  //exports json = json;
+	// 	  res.send(json)
+	// 	} catch (error) {
+	// 	  console.log(error);
+	// 	}
+	//   };
+	
+	//const weatherres = await getData(url);
+	//res.send(weatherres);
+	//console.log(weatherres);
+	// const response = await fetch(url);
+	// const json = await response.json();
+	// console.log(json);
+	// res.send(json
+	// 	// {
+	// 	// 	name: json.name, 
+	// 	// 	dt: json.dt, 
+	// 	// 	temp: json.main.temp,
+	// 	// 	feels_like: json.main.feels_like,   
+	// 	// 	sunrise: json.sys.sunrise, 
+	// 	// 	sunset: json.sys.sunset
+	// 	//  }
+	// 	)
+
+})
+
 
 
 // GET current weather from api + save in mongo 
@@ -26,7 +97,7 @@ apiRouter.route('/weather-api/public/weather/:name')
 .post(async function(req, res, next){
 	console.log("+++++++++++++++ Dans methode get and save current weather+++++++++++++++++++++++")
 	var cityName = req.params.name;
-	const fetch = require("node-fetch");
+	
 	const url = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=2d0c2252a4bc29b72d473be3efad81e1&units=metric";
 	const response = await fetch(url);
 	const json = await response.json();
@@ -36,7 +107,7 @@ apiRouter.route('/weather-api/public/weather/:name')
 			name: json.name, 
 			dt: json.dt, 
 			temp: json.main.temp,
-			feels_like: json.main.feels_like, 
+			feels_like: json.main.feels_like,   
 			sunrise: json.sys.sunrise, 
 			sunset: json.sys.sunset
 		 })} )
